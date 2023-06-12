387edfdd162553e2b89e0513e13b6a0a0c3ae0e1
Ryan Park <ryrypark@gmail.com>
2023-06-12 16:30:15 -0700

    add queue and stack ds

---
+++ stack.js
@@ -0,0 +1,55 @@
+/** Node: node for a stack. */
+
+class Node {
+  val = null;
+  next = null;
+
+  constructor(val) {
+    this.val = val;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  top = null;
+  size = 0;
+
+  /** push(val): add new value to the top of the stack. Returns undefined. */
+
+  push(val) {
+    let newNode = new Node(val);
+    newNode.next = this.top;
+
+    this.top = newNode;
+    this.size++;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if (this.size === 0) throw new Error();
+
+    let prevTopNode = this.top;
+    this.top = this.top.next;
+    this.size--;
+
+    return prevTopNode.val;
+  }
+
+  /** peek(): return the value of the top node in the stack. */
+
+  peek() {
+    return this.top.val
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0
+  }
+}
+
+module.exports = Stack;
